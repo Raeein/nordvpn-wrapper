c8e046d5025433836aa22a56e8b3a29af16401a4
Raeein Bagheri <raeeinbagheri80@gmail.com>
2024-12-03 21:04:51 -0500

    added a half decent gui

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::process::Command;
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let main_window = MainWindow::new()?;
 
+    main_window.on_check_if_pair_solved(move || {
+        println!("yo i got clicked");
+        let output = Command::new("ls")
+            .arg("-a")
+            .output()
+            .expect("ls command failed");
+        println!("status: {}", output.status);
+        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+        println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    });
     main_window.run()
 }
